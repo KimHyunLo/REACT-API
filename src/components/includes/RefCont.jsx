1b6592f76deb9ada5b16e32bc3e9ee3292b21198
unknown <leeas016@mgmail.com>
2022-09-25 12:57:09 +0900

    youtube api

--- src/components/includes/RefCont.jsx
+++ src/components/includes/RefCont.jsx
@@ -1,27 +1,39 @@
 import React from "react";
 
-function ReferItem(){
-
+function ReferItem({ id, title, desc, use }) {
+  return (
+    <li>
+      <a href="">
+        <span className="num">{id}</span>
+        <span className="title">{title}</span>
+        <span className="desc">{desc}</span>
+        <span className="use">{use}</span>
+      </a>
+    </li>
+  );
 }
 
-function RefCont({ refers }) {
+function ReferCont({ refers }) {
   return (
     <section className="refer__cont">
       <div className="container">
-        <div className="refer_inner">
+        <div className="refer__inner">
           <h2>CSS</h2>
-          <div className="refer__list">
-            {refers.map(refer=>(
+          <ul className="refer__list">
+            {refers.map((refer) => (
               <ReferItem
                 key={refer.id}
-                
+                id={refer.id}
+                title={refer.title}
+                desc={refer.desc}
+                use={refer.use}
               />
             ))}
-          </div>
+          </ul>
         </div>
       </div>
     </section>
   );
 }
 
-export default RefCont;
+export default ReferCont;
--- src/components/pages/Youtube.jsx
+++ src/components/pages/Youtube.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../layout/Header";
 import Footer from "../layout/Footer";
 import Content from "../layout/Content";
 import Title from "../layout/Title";
 import YoutubeCont from "../includes/YoutubeCont";
 import Contact from "../layout/Contact";
+import { useEffect } from "react";
 
 function Youtube() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=webstoryboy&maxResults=30&key=AIzaSyA8SlPA6kqX0OihTlnTbSNz8HL0gULdzB0",
+      requestOptions
+    )
+      .then((response) => response.json())
+      // .then((result) => console.log(result.items))
+      .then((result) => {
+        setVideos(result.items);
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+
   return (
     <>
       <Header />
       <Content>
         <Title title={["Youtube", "Api"]} />
-        <YoutubeCont />
+        <YoutubeCont videos={videos} />
         <Contact />
       </Content>
       <Footer />
